36e797630fc3457ab26273b63faf9d7e965bd851
jaso69 <joseonieva@gmail.com>
2024-02-25 19:59:31 +0100

    correction img source

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const esquemasImg = document.querySelector('#esquemasImg')
 
 const descSoft = document.querySelector('#descSoft')
 
-sourceImg.classList.add('cerrar')
+//sourceImg.classList.add('cerrar')
 layoutImg.classList.add('cerrar')
 correctionImg.classList.add('cerrar')
 processingImg.classList.add('cerrar')
@@ -28,56 +28,66 @@ monitorImg.classList.add('cerrar')
 presetImg.classList.add('cerrar')
 esquemasImg.classList.add('cerrar')
 
-esquemasButton.addEventListener('mouseover', () => { 
+esquemasButton.addEventListener('mouseover', () => {
+    sourceImg.classList.add('cerrar') 
     esquemasImg.classList.remove('cerrar')
     esquemasImg.classList.add('abrir')
     descSoft.classList.remove('abrir')
     descSoft.classList.add('cerrar') 
 })
 
-esquemasButton.addEventListener('mouseout', () => { 
+esquemasButton.addEventListener('mouseout', () => {
+    sourceImg.classList.add('cerrar') 
     esquemasImg.classList.remove('abrir')
     esquemasImg.classList.add('cerrar')
     descSoft.classList.add('abrir')
     descSoft.classList.remove('cerrar')  
 })
 
-presetButton.addEventListener('mouseover', () => { 
+presetButton.addEventListener('mouseover', () => {
+    sourceImg.classList.add('cerrar') 
     presetImg.classList.remove('cerrar')
     presetImg.classList.add('abrir') 
 })
 
-presetButton.addEventListener('mouseout', () => { 
+presetButton.addEventListener('mouseout', () => {
+    sourceImg.classList.add('cerrar') 
     presetImg.classList.remove('abrir')
     presetImg.classList.add('cerrar') 
 })
 
-monitorButton.addEventListener('mouseover', () => { 
+monitorButton.addEventListener('mouseover', () => {
+    sourceImg.classList.add('cerrar') 
     monitorImg.classList.remove('cerrar')
     monitorImg.classList.add('abrir') 
 })
 
-monitorButton.addEventListener('mouseout', () => { 
+monitorButton.addEventListener('mouseout', () => {
+    sourceImg.classList.add('cerrar') 
     monitorImg.classList.remove('abrir')
     monitorImg.classList.add('cerrar') 
 })
 
-screenButton.addEventListener('mouseover', () => { 
+screenButton.addEventListener('mouseover', () => {
+    sourceImg.classList.add('cerrar') 
     screenImg.classList.remove('cerrar')
     screenImg.classList.add('abrir') 
 })
 
-screenButton.addEventListener('mouseout', () => { 
+screenButton.addEventListener('mouseout', () => {
+    sourceImg.classList.add('cerrar') 
     screenImg.classList.remove('abrir')
     screenImg.classList.add('cerrar') 
 })
 
 processingButton.addEventListener('mouseover', () => { 
+    sourceImg.classList.add('cerrar')
     processingImg.classList.remove('cerrar')
     processingImg.classList.add('abrir') 
 })
 
-processingButton.addEventListener('mouseout', () => { 
+processingButton.addEventListener('mouseout', () => {
+    sourceImg.classList.add('cerrar') 
     processingImg.classList.remove('abrir')
     processingImg.classList.add('cerrar') 
 })
@@ -92,22 +102,26 @@ sourceButton.addEventListener('mouseout', () => {
     sourceImg.classList.add('cerrar') 
 })
 
-layoutButton.addEventListener('mouseover', () => { 
+layoutButton.addEventListener('mouseover', () => {
+    sourceImg.classList.add('cerrar') 
     layoutImg.classList.remove('cerrar')
     layoutImg.classList.add('abrir') 
 })
 
-layoutButton.addEventListener('mouseout', () => { 
+layoutButton.addEventListener('mouseout', () => {
+    sourceImg.classList.add('cerrar') 
     layoutImg.classList.remove('abrir')
     layoutImg.classList.add('cerrar') 
 })
 
-correctionButton.addEventListener('mouseover', () => { 
+correctionButton.addEventListener('mouseover', () => {
+    sourceImg.classList.add('cerrar') 
     correctionImg.classList.remove('cerrar')
     correctionImg.classList.add('abrir') 
 })
 
-correctionButton.addEventListener('mouseout', () => { 
+correctionButton.addEventListener('mouseout', () => {
+    sourceImg.classList.add('cerrar') 
     correctionImg.classList.remove('abrir')
     correctionImg.classList.add('cerrar') 
 })
