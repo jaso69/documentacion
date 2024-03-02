c34650fd13acc34c109fe46c9f9c032ed6561f0c
jaso69 <joseonieva@gmail.com>
2024-03-02 09:56:12 +0100

    print-scale

--- print.js
+++ print.js
@@ -1,9 +1,15 @@
 const el = document.querySelector('#boton')
    el.addEventListener('click', () => {
+      document.querySelector('#boton').style.display = 'none'
+      document.querySelector('#volver').style.display = 'none'
       const printSection = document.createElement('div')
-      var elemToPrint = document.querySelector('#img');
+      document.querySelector('div').style.display = 'flex'
+      document.querySelector('div').style.height = '90vh'
+      const elemToPrint = document.querySelector('#img');
       printSection.innerHTML = elemToPrint
       let domClone = elemToPrint.cloneNode(true);
       printSection.appendChild(domClone);
-      window.print(); //mando a imprimir
+      window.print(); 
+      document.querySelector('#boton').style.display = 'flex'
+      document.querySelector('#volver').style.display = 'flex'
    })
